a1413d357e964ad55c175ba02e1d7fbd54171cff
Dino Morelli <dino@ui3.info>
2020-11-27 10:23:05 -0500

    Added license and updated install script

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-PREFIX=${PREFIX:-"dist/usr"}
+version="1.0"
+
+PREFIX=${PREFIX:-"systemd-failmsg-${version}"}
+
+shareDir="$PREFIX/share/systemd-failmsg"
 
 install -Dm0755 failmsg.sh "$PREFIX/bin/failmsg.sh"
 install -Dm0644 "failmsg@.service" "$PREFIX/lib/systemd/system/failmsg@.service"
+install -Dm0644 "LICENSE" "${shareDir}/doc/LICENSE"
+install -Dm0644 "README.md" "${shareDir}/doc/README.md"
