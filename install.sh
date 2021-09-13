7abd6e8e66e29be6cd685365cd602cb4ad865e64
Dino Morelli <dino@ui3.info>
2021-09-13 16:29:21 -0400

    Added the conf changes for default behavior
    This changes the installation so that all services on the system will
    default to using it without an explicit OnFailure set.

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@
 version="1.3"
 
 PREFIX=${PREFIX:-"systemd-failmsg-${version}"}
-
+unitDir="$PREFIX/lib/systemd/system"
 shareDir="$PREFIX/share/systemd-failmsg"
 
 install -Dm0755 failmsg.sh "$PREFIX/bin/failmsg.sh"
-install -Dm0644 "failmsg@.service" "$PREFIX/lib/systemd/system/failmsg@.service"
+install -Dm0644 "failmsg@.service" "$unitDir/failmsg@.service"
+install -Dm0644 service.d_toplevel-override.conf "$unitDir/service.d/toplevel-override.conf"
+install -Dm0644 failmsg@.service.d_toplevel-override.conf "$unitDir/failmsg@.service.d/toplevel-override.conf"
 install -Dm0644 "LICENSE" "${shareDir}/doc/LICENSE"
 install -Dm0644 "README.md" "${shareDir}/doc/README.md"
